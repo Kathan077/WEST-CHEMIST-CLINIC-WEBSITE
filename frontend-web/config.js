0ce9adeddf7584a8c2e6d7d9f6f8281eadc8ee98
Kathan Patel <kathanpatel099@gmail.com>
2026-06-04 11:54:05 +0530

    add

--- frontend-web/config.js
+++ frontend-web/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://west-chemist-clinic-website.onrender.com';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:5000';
